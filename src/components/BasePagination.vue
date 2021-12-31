ad5e1298d33c813d4953a1f94f35cd81412d8311
Haf <haf@protonmail.ch>
2021-12-31 23:17:19 +0100

    better pagination logic

--- src/components/BasePagination.vue
+++ src/components/BasePagination.vue
@@ -2,42 +2,52 @@
 <nav class="pagination" role="navigation" aria-label="pagination">
   <a class="pagination-previous">Previous</a>
   <a class="pagination-next">Next</a>
-  <ul class="pagination-list">
-    <base-pagination-link 
-      :pageNumber="1" 
-      :currentPage="currentPage"
-    />
-    
-    <base-pagination-link v-for="page in showBackward"
-      :pageNumber="page"
-      :currentPage="currentPage"
-      :key="`page-${page}`"
-    />
+  <template v-if="pageNumber > 0">
+    <ul class="pagination-list">
+      <base-pagination-link 
+        v-if="!backwardReachesFirst"
+        :pageNumber="1" 
+        :currentPage="currentPage"
+      />
 
-    <base-pagination-link
-      :pageNumber="currentPage"
-      :currentPage="currentPage"
-    />
+      <li v-if="!backwardReachesFirst">
+        <span class="pagination-ellipsis">&hellip;</span>
+      </li>
+      
+      <base-pagination-link v-for="page in showBackward"
+        :pageNumber="page"
+        :currentPage="currentPage"
+        :key="`page-${page}`"
+      />
 
-    <base-pagination-link v-for="page in showForward"
-      :pageNumber="page"
-      :currentPage="currentPage"
-      :key="`page-${page}`"
-    />
+      <base-pagination-link
+        :pageNumber="currentPage"
+        :currentPage="currentPage"
+      />
 
-    <base-pagination-link v-if="pageNumber > 1"
-      :pageNumber="pageNumber"
-      :currentPage="currentPage"
-    />
-  </ul>
+      <base-pagination-link v-for="page in showForward"
+        :pageNumber="page"
+        :currentPage="currentPage"
+        :key="`page-${page}`"
+      />
+
+      <li v-if="!forwardReachesLast">
+        <span class="pagination-ellipsis">&hellip;</span>
+      </li>
+
+      <base-pagination-link 
+        v-if="!forwardReachesLast"
+        :pageNumber="pageNumber"
+        :currentPage="currentPage"
+      />
+    </ul>
+  </template>
 </nav>
 </template>
 
 <script setup lang="ts">
 // TODO: Icons for previous and next?
 // TODO: Provides and inject for current page? And also emitting goto
-// TODO: Ellipsis..es
-// TODO: Edge cases: backward and forward are fewer than to first and last, 0 pages
 // TODO: Validation that current page must be between 1 and last page?
 // TODO: Obviously, goto page events!
 // TODO: Test it!
@@ -63,13 +73,21 @@ const showBackward = computed(() => {
 });
 
 const showForward = computed(() => {
-  const last = Math.min(props.pageNumber, props.currentPage + props.showAround);
+  const last = Math.min(props.pageNumber, props.currentPage + props.showAround + 1);
   const pagesToShow: number[] = [];
 
-  for (let page = last; page > props.currentPage; page--)
+  for (let page = props.currentPage + 1; page <= last; page++)
     pagesToShow.push(page);
 
   return pagesToShow;
+});
+
+const backwardReachesFirst = computed(() => {
+  return showBackward.value.includes(1) || showBackward.value.length === 0;
+});
+
+const forwardReachesLast = computed(() => {
+  return showForward.value.includes(props.pageNumber) || showForward.value.length === 0;
 });
 
 </script>
