e9ea36a112588a291ea113d2229780a4925f3076
Haf <haf@protonmail.ch>
2021-12-31 23:28:06 +0100

    icons for previous and next pages

--- src/components/BasePagination.vue
+++ src/components/BasePagination.vue
@@ -1,7 +1,11 @@
 <template>
 <nav class="pagination" role="navigation" aria-label="pagination">
-  <a class="pagination-previous">Previous</a>
-  <a class="pagination-next">Next</a>
+  <a class="pagination-previous" aria-label="previous page">
+    <font-awesome-icon icon="angle-left"/>
+  </a>
+  <a class="pagination-next" aria-label="next page">
+    <font-awesome-icon icon="angle-right"/>
+  </a>
   <template v-if="pageNumber > 0">
     <ul class="pagination-list">
       <base-pagination-link 
@@ -46,13 +50,18 @@
 </template>
 
 <script setup lang="ts">
-// TODO: Icons for previous and next?
 // TODO: Provides and inject for current page? And also emitting goto
 // TODO: Validation that current page must be between 1 and last page?
 // TODO: Obviously, goto page events!
 // TODO: Test it!
 import { computed } from 'vue';
 import BasePaginationLink from '@/components/BasePaginationLink.vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faAngleLeft);
+library.add(faAngleRight);
 
 const props = withDefaults(defineProps<{
   pageNumber: number;
