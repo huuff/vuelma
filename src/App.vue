6655b44af4396e18f14318d0cc704c3eae89af00
Haf <haf@protonmail.ch>
2022-01-01 13:29:53 +0100

    removed classed slot example

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
   </template>
   <template #end>
     <router-link to="/card">Card</router-link>
-    <router-link to="/classed-slot">Classed Slot</router-link>
     <router-link to="/breadcrumb">Breadcrumb</router-link>
     <router-link to="/dropdown">Dropdown</router-link>
     <router-link to="/message">Message</router-link>
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import ExampleCard from '@/pages/ExampleCard.vue';
-import ExampleClassedSlot from '@/pages/ExampleClassedSlot.vue';
 import ExampleBreadcrumb from '@/pages/ExampleBreadcrumb.vue';
 import ExampleDropdown from '@/pages/ExampleDropdown.vue';
 import ExampleMessage from '@/pages/ExampleMessage.vue';
@@ -12,7 +11,6 @@ import ExampleHome from '@/pages/ExampleHome.vue';
 const routes = [
   { path: "/", component: ExampleHome, name: "Home" },
   { path: "/card", component: ExampleCard, name: "Card" },
-  { path: "/classed-slot", component: ExampleClassedSlot, name: "Classed Slot" },
   { path: "/breadcrumb", component: ExampleBreadcrumb, name: "Breadcrumb" },
   { path: "/dropdown", component: ExampleDropdown, name: "Dropdown" },
   { path: "/message", component: ExampleMessage, name: "Message"},
