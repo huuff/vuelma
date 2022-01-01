83d42ecaa49f729f2bb47b6c166cf320d5a19186
Haf <haf@protonmail.ch>
2022-01-01 09:21:53 +0100

    more pagination testing

--- tests/unit/BasePagination.spec.ts
+++ tests/unit/BasePagination.spec.ts
@@ -9,9 +9,10 @@ function findPageAnchorByNumber(wrapper: VueWrapper, num: number): DOMWrapper<HT
 describe("BasePagination.vue", () => {
   describe("10 pages, current page 5", () => {
     const currentPage = 5;
+    const pageNumber = 10;
     const wrapper = mount(BasePagination, {
       props: {
-        pageNumber: 10,
+        pageNumber,
         currentPage,
         showAround: 2,
       }, global: {
@@ -23,8 +24,20 @@ describe("BasePagination.vue", () => {
       expect(findPageAnchorByNumber(wrapper, currentPage).element.classList).toContain("is-current");
     });
 
-    test("4 is shown", () => {
+    test("numbers around are shown", () => {
       expect(findPageAnchorByNumber(wrapper, 4).exists()).toBeTrue();
+      expect(findPageAnchorByNumber(wrapper, 3).exists()).toBeTrue();
+      expect(findPageAnchorByNumber(wrapper, 6).exists()).toBeTrue();
+      expect(findPageAnchorByNumber(wrapper, 7).exists()).toBeTrue();
     })
+
+    test("first and last are shown", () => {
+      expect(findPageAnchorByNumber(wrapper, 1).exists()).toBeTrue();
+      expect(findPageAnchorByNumber(wrapper, pageNumber).exists()).toBeTrue();
+    })
+
+    test("there are two ellipses", () => {
+      expect(wrapper.findAll('.pagination-ellipsis').length).toBe(2);
+    });
   });
 });
