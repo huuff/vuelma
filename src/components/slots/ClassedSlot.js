ba52ad49eba9b06b238e380e931548553c2ff368
Haf <haf@protonmail.ch>
2022-01-01 21:36:20 +0100

    fixed dropdown

--- src/components/slots/ClassedSlot.js
+++ src/components/slots/ClassedSlot.js
@@ -1,15 +1,18 @@
 import { cloneVNode } from 'vue';
 
 // FUTURE: Wish I could type this but it's extremely hard for me
-function recursivelyAddClass(element, classToAdd) {
+function recursivelyAddClass(element, classToAdd, excluded) {
   if (Array.isArray(element)) {
-    return element.map(el => recursivelyAddClass(el, classToAdd));
+    return element.map(el => recursivelyAddClass(el, classToAdd, excluded));
   } else if (element.type.toString() === 'Symbol(Fragment)') {
     const clone = cloneVNode(element);
-    clone.children = recursivelyAddClass(element.children, classToAdd)
+    clone.children = recursivelyAddClass(element.children, classToAdd, excluded)
     return clone;
   } else {
-    return cloneVNode(element, { class: classToAdd });
+    if (!excluded || !excluded.includes(element.type))
+      return cloneVNode(element, { class: classToAdd });
+    else
+      return element;
   }
 }
 
@@ -18,6 +21,9 @@ export default {
     childrenClass: {
       type: String,
       required: true
+    },
+    excludedElements: { // Do not apply the class to these elements
+      type: Array
     },
   },
 
