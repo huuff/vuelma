1f85e6d034c34953328ccaecd25c37d5b1a457c6
Haf <haf@protonmail.ch>
2022-01-01 10:18:43 +0100

    starting with base panel

---
+++ src/components/panel/BasePanel.vue
@@ -0,0 +1,18 @@
+<template>
+<article :class="`panel is-${color}`">
+  <p v-if="title"
+    class="panel-heading"
+  > {{ title }}</p>
+</article>
+</template>
+
+<script setup lang="ts">
+import { BulmaColor } from '@/types/bulma-color';
+
+const props = withDefaults(defineProps<{
+  color?: BulmaColor;
+  title?: string;
+}>(), {
+  color: 'dark',
+});
+</script>
---
+++ src/components/panel/BasePanelBlock.vue
@@ -0,0 +1,22 @@
+<template>
+<a 
+  class="panel-block" 
+  :class="{ 'is-active': active }"
+  >
+  <span v-if="icon"
+    class="panel-icon"
+  >
+    <font-awesome-icon :icon="icon" aria-hidden="true"/>
+  </span>
+  <slot></slot>
+</a>
+</template>
+
+<script setup lang="ts">
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome';
+
+const props = defineProps<{
+  icon?: string | [string, string];
+  active?: boolean;
+}>();
+</script>
