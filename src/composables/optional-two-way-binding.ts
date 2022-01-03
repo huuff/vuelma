1ec2ec2f53e6176d34c186103784930223736c3a
Haf <haf@protonmail.ch>
2022-01-03 18:42:29 +0100

    silly me, that's not possible because they are compiler macros

--- src/composables/optional-two-way-binding.ts
+++ src/composables/optional-two-way-binding.ts
@@ -6,7 +6,6 @@ import { ref, computed, UnwrapRef, Ref } from 'vue';
 // defaultValue: For the internal state, if none is provided
 // fromProps: Get the provided state, or undefined if there is none
 // emitUpdate: An emit, for notifying parent of state changes (if state was provided)
-// TODO: Automatically create it from the props, the emits and the name of the prop?
 export function useOptionalTwoWayBinding<T>(defaultValue: T, fromProps: Ref<T>, emitUpdate: (newVal: T) => void) {
   const internal = ref(defaultValue);
 
