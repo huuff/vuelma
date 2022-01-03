ef6e73e030399ca4d4ce0a59e09fc6871f274dfa
Haf <haf@protonmail.ch>
2022-01-03 22:46:00 +0100

    active tab setting

--- src/components/tabs/BaseTabs.vue
+++ src/components/tabs/BaseTabs.vue
@@ -3,15 +3,43 @@
 </template>
 
 <script setup lang="ts">
-import { useSlots, h, VNode } from 'vue';
+import { useSlots, h, VNode, toRef } from 'vue';
+import { useOptionalTwoWayBinding } from '@/composables/optional-two-way-binding';
 import BaseTab from './BaseTab.vue';
+import partial from "lodash/partial";
+
+
+const props = defineProps<{
+  activeTabId?: string;
+}>();
+
+const emit = defineEmits<{
+  (event: "update:activeTabId", tabId?: string): void;
+}>();
+
+const actualActiveTabId = useOptionalTwoWayBinding(undefined, toRef(props, "activeTabId"), partial(emit, "update:activeTabId"));
 
 const slots = useSlots();
 
-interface Tab {
-  title: string;
-  tabId: string;
-  slot: VNode<any, any, any>;
+// TODO: This in an external file? But it uses the actualActiveTabId
+class Tab {
+  constructor(
+    public readonly title: string,
+    public readonly tabId: string,
+    public readonly slot: VNode<any, any, any>,
+  ) {}
+
+  public getClasses(): { class: string } | Record<string, never> {
+    if (this.tabId === actualActiveTabId.value) {
+      return { class: "is-active"}; 
+    } else {
+      return {}
+    }
+  }
+
+  public setActive(): void {
+    actualActiveTabId.value = this.tabId;
+  }
 }
 
 function slotToTabs(): Tab[] {
@@ -23,11 +51,11 @@ function slotToTabs(): Tab[] {
     }
   }
 
-  return children.map(child => ({
-    title: child.props!.title,
-    tabId: child.props!.tabId,
-    slot: child,
-  }));
+  return children.map(child => new Tab(
+    child.props!.title,
+    child.props!.tabId,
+    child,
+  ));
 }
 
 const render = () => {
@@ -42,10 +70,10 @@ const render = () => {
         {},
         tabs.map(tab => h(
           "li",
-          {},
+          tab.getClasses(),
           h(
             "a",
-            {},
+            { onClick: () => tab.setActive()},
             tab.title
           )
         ))
