38498c7a05512e218b33dbc9e0b301bf35eb4496
Haf <haf@protonmail.ch>
2022-01-04 12:32:06 +0100

    tested another little BaseTabs.vue

--- tests/unit/BaseTabs.spec.ts
+++ tests/unit/BaseTabs.spec.ts
@@ -12,8 +12,8 @@ describe('BaseTabs.vue', () => {
       },
       slots: {
         default: `
-          <base-tab title="Test" tabId="test1">
-            Tab contents
+          <base-tab title="Test1" tabId="test1">
+            Tab contents 1
           </base-tab>
           <div>
             This is a non-tab child, so it should throw
@@ -24,5 +24,35 @@ describe('BaseTabs.vue', () => {
         `,
       },
     }));
+  });
+  test("the titles of the tabs appear in the tab elements", () => {
+    const tab1Title = "Test1";
+    const tab2Title = "Test2";
+    const tab3Title = "Test3";
+    const wrapper = mount(BaseTabs, {
+      global: {
+        components: {
+          'base-tab': BaseTab,
+        },
+      },
+      slots: {
+        default: `
+        <base-tab title="${tab1Title}" tabId="test1">
+          Tab contents 1
+        </base-tab>
+        <base-tab title="${tab2Title}" tabId="test2">
+          Tab contents 2
+        </base-tab>
+        <base-tab title="${tab3Title}" tabId="test3">
+          Tab contents 3
+        </base-tab>
+        `,
+      },
+    });
+
+    const allTabs = wrapper.get("ul").findAll("li");
+    expect(allTabs[0].text()).toBe(tab1Title);
+    expect(allTabs[1].text()).toBe(tab2Title);
+    expect(allTabs[2].text()).toBe(tab3Title);
   });
 });
